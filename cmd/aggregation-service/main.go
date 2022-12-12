07ed1d1e49c77710022cc1f25625539b64d1f2b6
Daniel Linares <daniel.linares@ciencias.unam.mx>
2022-12-12 15:33:07 -0600

    Implement aggregation service

--- cmd/aggregation-service/main.go
+++ cmd/aggregation-service/main.go
@@ -1,24 +1,92 @@
 package main
 
 import (
-	"io"
-	"log"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/D4niel44/upfluence-challenge/internal/aggregations"
 )
 
+const (
+	port = ":8080"
+)
+
+var sseService = &SSEService{
+	url:        "https://stream.upfluence.co/stream",
+	numWorkers: 10000,
+	timeout:    5 * time.Second,
+}
+
 func getAnalysis(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got request")
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusNotFound)
-		io.WriteString(w, "404 page not found")
 		return
 	}
 
-	log.Println("Got Request")
-	io.WriteString(w, "Placeholder.\n")
+	durationString := r.URL.Query().Get("duration")
+	if durationString == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	dimension := r.URL.Query().Get("dimension")
+	if dimension == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	duration, err := time.ParseDuration(durationString)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "no-cache")
+
+	in := make(chan map[string]interface{})
+	broadcastService.AddClient(in)
+	defer broadcastService.RemoveClient(in)
+	aggregatorState := aggregations.InitialState(dimension)
+	timeout := time.After(duration)
+loop:
+	for {
+		select {
+		case <-timeout:
+			break loop
+		case post := <-in:
+			err := aggregatorState.Aggregate(post)
+			if err != nil {
+				WarningLogger.Println(err)
+				WarningLogger.Println(post)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+	if aggregatorState.TotalPosts == 0 {
+		aggregatorState.MinTimestamp = 0
+		aggregatorState.MaxTimestamp = 0
+	}
+	w.Write(EncodeResponse(aggregatorState))
+}
+
+func EncodeResponse(aggregation *aggregations.AggregatorState) []byte {
+	response := make(map[string]interface{})
+	response["total_posts"] = aggregation.TotalPosts
+	response["minimum_timestamp"] = aggregation.MinTimestamp
+	response["maximum_timestamp"] = aggregation.MaxTimestamp
+	response["avg_"+aggregation.Dimension] = aggregation.DimensionAvg
+	responseBlob, _ := json.Marshal(response)
+	return responseBlob
 }
 
 func main() {
-	http.HandleFunc("/analysis", getAnalysis)
+	sseService.startSSEService()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/analysis", getAnalysis)
+	ErrorLogger.Fatal(http.ListenAndServe(port, nil))
 }
---
+++ cmd/aggregation-service/sseserver.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"time"
+
+	"github.com/D4niel44/upfluence-challenge/internal/broadcast"
+	"github.com/D4niel44/upfluence-challenge/internal/sse"
+)
+
+type SSEService struct {
+	url        string
+	numWorkers int
+	timeout    time.Duration
+}
+
+func parseJsonEvent(eventBlob []byte) (eventData map[string]interface{}) {
+	d := json.NewDecoder(bytes.NewReader(eventBlob))
+	d.UseNumber()
+	if err := d.Decode(&eventData); err != nil {
+		WarningLogger.Println("Error parsing json event: ", err)
+	}
+	return
+}
+
+var broadcastService = broadcast.NewBroadcastService(parseJsonEvent)
+
+func (config *SSEService) startSSEService() {
+	c := make(chan []byte, 1)
+
+	go func() {
+		for {
+			err := sse.StartListening(config.url, c)
+			if err != nil {
+				ErrorLogger.Println(err)
+			} else {
+				ErrorLogger.Println("Conection to sse server closed.")
+			}
+			time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			err := broadcastService.StartBroadcast(c, config.numWorkers, config.timeout)
+			if err != nil {
+				ErrorLogger.Println(err)
+			} else {
+				ErrorLogger.Println("Broadcast server crashed.")
+			}
+			time.Sleep(time.Second)
+		}
+	}()
+}
